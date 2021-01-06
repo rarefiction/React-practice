dbb136dd4786b8caa8a37be9e55cb96947d82a86
rarefiction <74932637+rarefiction@users.noreply.github.com>
2021-01-07 02:06:37 +1300

    Add files via upload
    first react app (keeper app)

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Footer() {
+  const date = new Date();
+  const year = date.getFullYear();
+
+  return (
+    <footer>
+      <p>Copyright {year}</p>
+    </footer>
+  );
+}
+
+export default Footer;
